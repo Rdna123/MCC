0170b44881328397e7e489ba2d761c40ec649ef3
Rdna123 <chknmanrob1904@gmail.com>
2020-12-26 21:19:12 -0800

    Initial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Link from 'next/link'
+// import Layout, { siteTitle } from '../components/layout'
+// import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return ("Hello")
+}
